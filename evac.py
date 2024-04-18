cece62b3945a793be35c0c7d21416c351c9bc58d
ADS_Fibonacci <56398573+adsfibonacci@users.noreply.github.com>
2024-04-18 04:08:17 -0400

    Add files via upload

--- evac.py
+++ evac.py
@@ -1,5 +1,8 @@
 import pulp
-import csv 
+import csv
+import random
+
+random.seed(100)
 
 # Define the Minimization Problem
 prob = pulp.LpProblem('wildfires', pulp.LpMinimize)
@@ -7,6 +10,10 @@ prob = pulp.LpProblem('wildfires', pulp.LpMinimize)
 # Define the number of each node
 evac_count = 31    #N
 transit_count = 10 #M
+
+total_pop = 15000
+region_pop = total_population/4
+evac_pop = region_population / evac_count
 
 # Define the graph network of both evacuation leaves to transits and intertransit travel
 evac_transit = []
@@ -24,10 +31,13 @@ with open('transits.csv', newline='') as f:
 
 transits = [[int(i) for i in row] for row in reader] #safe node is transit node 1
 
-# Start Node | End Node | Travel Time | Capacity
+# Start Node | End Node | Travel Time | Capacity | Unsafe Time (if possible)
 
 s = pulp.LpVariable.dicts("s", range(1, evac_count + 1), cat='Integer') # Should have evacuation node count size (N)
 h = pulp.LpVariable.dicts("h", range(1, evac_count + 1), cat='Integer') # Should have transit node count size (N)
+
+# FIXME edit the random length
+prob += max(s[i] + h[i] / evac_pop - min(transits[j][4] - random.randint(1, transits[j][4] + 1) for j in range(1, transit_count + 1)) for i in range(1, evac_count + 1))
 
 prob.solve()
 
